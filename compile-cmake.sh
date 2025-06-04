da0cdabdaf57bd055d39901aae412f34a542a901
shenweihai1 <shenweihai1@gmail.com>
2025-06-04 21:49:35 +0000

    migrate from makefile to cmakefile: read TODO in readme of this commit

---
+++ compile-cmake.sh
@@ -0,0 +1,12 @@
+rm -rf ./out-perf.masstree/*
+rm -rf ./src/warbler/out-perf.masstree/*
+cd build
+rm -rf CMakeFiles cmake_install.cmake CMakeCache.txt
+cmake .. -DDEBUG=ON -DPAXOS_LIB_ENABLED=0 -DDISABLE_MULTI_VERSION=0 \
+        -DMICRO_BENCHMARK=0 -DTRACKING_LATENCY=0 -DSHARDS=4 \
+        -DMERGE_KEYS_GROUPS=4 -DMEGA_BENCHMARK=0 \
+        -DFAIL_NEW_VERSION=1 -DTRACKING_ROLLBACK=0
+
+
+make -j32 dbtest VERBOSE=1
+# make -j32 basic VERBOSE=1
