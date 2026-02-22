a71052a38c45c0deb63975a69146713c57e0a1a9
Shuai Mu <msmummy@gmail.com>
2026-02-21 21:25:11 -0500

    Fail fast when dbtest binary is missing in 2-shard replication script

--- examples/test_2shard_replication.sh
+++ examples/test_2shard_replication.sh
@@ -22,6 +22,7 @@ rm -rf /tmp/${USERNAME}_mako_rocksdb_shard*
 
 trd=${1:-6}
 script_name="$(basename "$0")"
+binary_path="./${BUILD_DIR:-build}/dbtest"
 SHARD0_LOCALHOST_PID=""
 SHARD0_LEARNER_PID=""
 SHARD0_P2_PID=""
@@ -31,6 +32,12 @@ SHARD1_LEARNER_PID=""
 SHARD1_P2_PID=""
 SHARD1_P1_PID=""
 CLEANUP_DONE=0
+
+if [ ! -x "$binary_path" ]; then
+    echo "Error: dbtest binary not found or not executable at '$binary_path'"
+    echo "Build it first (for Docker: ./docker_build.sh build), then retry."
+    exit 1
+fi
 
 TEMP_CONFIG=$(make_simple_txn_rep_config 2 $trd)
 if [ -z "$TEMP_CONFIG" ]; then
