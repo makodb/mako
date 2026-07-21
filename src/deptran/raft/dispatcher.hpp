11951a94204074846c15c2d40fac6dc6302337b7
Frank <frankwangli0326@gmail.com>
2026-07-21 09:40:28 -0400

    raft: migrate dispatcher interface to inline rust dsl

--- src/deptran/raft/dispatcher.hpp
+++ src/deptran/raft/dispatcher.hpp
@@ -51,19 +51,44 @@ struct InstallSnapshotReply;
 // DispatcherBase / DispatcherProxy
 // ---------------------------------------------------------------------------
 
+#if RUSTYCPP_RUST
+pub trait DispatcherBase {
+    fn handle_vote(&mut self, req: VoteReq) -> VoteReply;
+    fn handle_vote_durable(&mut self, req: VoteDurableReq) -> VoteDurableReply;
+    fn handle_append_entries(&mut self, req: AppendEntriesReq) -> AppendEntriesReply;
+    fn handle_empty_append_entries(&mut self, req: EmptyAppendEntriesReq) -> EmptyAppendEntriesReply;
+    fn handle_append_entries_durable(&mut self, req: AppendEntriesDurableReq) -> AppendEntriesDurableReply;
+    fn handle_timeout_now(&mut self, req: TimeoutNowReq) -> TimeoutNowReply;
+    fn handle_notify_restart(&mut self, req: NotifyRestartReq) -> NotifyRestartReply;
+    fn handle_install_snapshot(&mut self, req: InstallSnapshotReq) -> InstallSnapshotReply;
+}
+#endif
+/*RUSTYCPP:GEN-BEGIN id=dispatcher.1 version=1 rust_sha256=f744d708debc2cdeb8bae874ca2b85401771572e6327b2c78b120cfc561ac365*/
+namespace {
 class DispatcherBase {
- public:
-  virtual ~DispatcherBase() = default;
-
-  virtual VoteReply                 handle_vote(VoteReq) = 0;
-  virtual VoteDurableReply          handle_vote_durable(VoteDurableReq) = 0;
-  virtual AppendEntriesReply        handle_append_entries(AppendEntriesReq) = 0;
-  virtual EmptyAppendEntriesReply   handle_empty_append_entries(EmptyAppendEntriesReq) = 0;
-  virtual AppendEntriesDurableReply handle_append_entries_durable(AppendEntriesDurableReq) = 0;
-  virtual TimeoutNowReply           handle_timeout_now(TimeoutNowReq) = 0;
-  virtual NotifyRestartReply        handle_notify_restart(NotifyRestartReq) = 0;
-  virtual InstallSnapshotReply      handle_install_snapshot(InstallSnapshotReq) = 0;
+public:
+    virtual ~DispatcherBase() noexcept(false) {}
+    virtual VoteReply handle_vote(VoteReq req) = 0;
+    virtual VoteDurableReply handle_vote_durable(VoteDurableReq req) = 0;
+    virtual AppendEntriesReply handle_append_entries(AppendEntriesReq req) = 0;
+    virtual EmptyAppendEntriesReply handle_empty_append_entries(EmptyAppendEntriesReq req) = 0;
+    virtual AppendEntriesDurableReply handle_append_entries_durable(AppendEntriesDurableReq req) = 0;
+    virtual TimeoutNowReply handle_timeout_now(TimeoutNowReq req) = 0;
+    virtual NotifyRestartReply handle_notify_restart(NotifyRestartReq req) = 0;
+    virtual InstallSnapshotReply handle_install_snapshot(InstallSnapshotReq req) = 0;
+    DispatcherBase(const DispatcherBase&) = delete;
+    DispatcherBase& operator=(const DispatcherBase&) = delete;
+    DispatcherBase(DispatcherBase&&) = delete;
+    DispatcherBase& operator=(DispatcherBase&&) = delete;
+protected:
+    DispatcherBase() = default;
 };
+}
+
+template <class U> class DispatcherBaseAdapter;
+template <class U> class DispatcherBaseAdapterRef;
+template <class U> class DispatcherBaseAdapterRefMut;
+/*RUSTYCPP:GEN-END id=dispatcher.1*/
 
 using DispatcherProxy = rusty::Box<DispatcherBase>;
 
